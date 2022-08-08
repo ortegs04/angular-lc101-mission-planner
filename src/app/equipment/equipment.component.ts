c460cde0939156a3487ed68c1c641b3e76f7df85
ortegs04 <stevenmortega72@gmail.com>
2022-08-08 18:30:17 -0500

    Equipment and First Commit

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-equipment',
@@ -21,11 +22,46 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
-
+   active: boolean = true;
+   cargoHoldLow: boolean = false;
+   remainingMass: number = 0;
+   holdIndex: number = 0;
+   itemWithIndex: object[] = [];
+  
    constructor() { }
-
    ngOnInit() { }
 
    // Code your addItem function here:
-   
+   addItem(item: object) {
+
+     this.cargoHold.push(item);
+     this.cargoMass += item["mass"];
+     this.remainingMass = this.maximumAllowedMass - this.cargoMass;
+     this.holdIndex += 1;
+     
+     this.itemWithIndex.push( {obj: item, index: this.holdIndex} )
+
+     if (this.remainingMass > 200) return false;
+     if (this.remainingMass <= 200) return true;
+
+   }
+
+   emptyHold() {
+     this.cargoHold = [];
+     this.cargoHoldLow = false;
+     this.cargoMass = 0;
+     this.holdIndex = 0;
+   }
+
+   haveTwoInHold(item: object) {
+     let result = this.cargoHold.filter(obj => obj === item);
+     let length = result.length;
+     if (length === 2) return true;
+     return false; 
+   }
+
+   removeItem(item: object) {
+     let result = this.itemWithIndex.filter(stuff => stuff !== item);
+     this.itemWithIndex = result;
+   }
 }
